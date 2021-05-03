2b0ebfc4f40a08b9f9ddfa4eb8066cdec756b336
swapecoevo <sw.subramanian@gmail.com>
2021-05-03 15:43:01 -0400

    vsearch clustering script

---
+++ 05_Clustering/vsearch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=vsearch
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --mem=50G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=amanda.pastore@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+##################################################
+##  	Clustering using vsearch		##
+##################################################
+module load vsearch/2.4.3
+
+vsearch --threads 8 --log LOGFile \
+	--cluster_fast ../04_Coding_Regions/trinity_combine.fasta.transdecoder.cds \
+	--id 0.90 \
+	--centroids centroids.fasta \
+	--uc clusters.uc
+
+date 
+
